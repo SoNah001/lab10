21e53089276a87daacf8b5ef78a126d2c6255d2d
Supanat <k.suphanuch23@gmail.com>
2024-12-20 23:04:49 +0700

    Lap10_3 complete

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,48 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    ifstream source("score.txt");
+    if (!source) {
+        cerr << "Error: Cannot open file score.txt\n";
+        return 1;
+    }
+    
+    vector<double> scores;
+    double value;
+
+    while (source >> value) {
+        scores.push_back(value);
+    }
+    source.close();
+
+    int n = scores.size();
+    if (n == 0) {
+        cerr << "Error: No data in file.\n";
+        return 1;
+    }
+
+    double sum = 0;
+    for (double score : scores) {
+        sum += score;
+    }
+    double mean = sum / n;
+
+    double sum_of_squares = 0;
+    for (double score : scores) {
+        sum_of_squares += pow(score - mean, 2);
+    }
+    double stddev = sqrt(sum_of_squares / n);
+
+    cout << "Number of data = " << n << "\n";
+    cout << setprecision(3);
+    cout << "Mean = " << mean << "\n";
+    cout << "Standard deviation = " << stddev << "\n";
+
+    return 0;
+}
